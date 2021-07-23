ae974584452e0ce6f8d76b1e394c3b5c75608364
Christian Smucker <chrissmucker94@gmail.com>
2021-07-23 16:29:36 -0600

    Add function for meal selection

--- main.js
+++ main.js
@@ -7,20 +7,38 @@ var clearBtn = document.getElementById('clear');
 var burgerImage = document.getElementById('image');
 var displayRecipe = document.getElementById('dish');
 
+var crockPot = document.querySelector('.img1');
+
+var showRecipe = document.getElementById('showRecipe');
+
 cookBtn.addEventListener('click', letsCook);
-sidesBtn.addEventListener('click', );
-dessertBtn.addEventListener('click', );
-mainBtn.addEventListener('click', );
+sidesBtn.addEventListener('click', dishBtn);
+dessertBtn.addEventListener('click', dishBtn);
+mainBtn.addEventListener('click', dishBtn;
 recipeBtn.addEventListener('click', makeDish);
 clearBtn.addEventListener('click', clearDish);
 burgerImage.addEventListener('click', imageFade);
 displayRecipe.addEventListener('click', showDish);
 
 
-
-
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+function makeDish() {
+  var side = sides[getRandomIndex(sides)];
+  var main = mains[getRandomIndex(mains)];
+  var dessert = desserts[getRandomIndex(desserts)];
+  crockPot.classList.toggle('hidden');
+  clearBtn.classList.toggle('fadeIn');
+  if (sidesBtn.checked) {
+    showRecipe.innerHTML = `<h4>This is your side: ${side}</h4>`
+  } else if (mainBtn.checked) {
+    showRecipe.innerHTML = `<h4>This is your main dish: ${main}</h4>`
+  } else (dessert.Btn){
+    showRecipe.innerHTML = `<h4>This is your dessert: ${dessert}</h4>`
+  }
+}
+
 
 //
